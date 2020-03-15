a26a36ae44b9c1a644972ef120df14f8fe7651f5
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-15 14:26:45 -0500

    function _printf

--- _printf.c
+++ _printf.c
@@ -6,46 +6,27 @@
  */
 int _printf(const char *format, ...)
 {
-	argum fm[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{NULL, NULL}
-	};
+		int cont = 0, i;
+
 	va_list formato;
 
 	va_start(formato, format);
 
-	int i, j;
-
-	i = 0;
-
-	while (format && format[i])
+	if (format == NULL)
+		return (-1);
+	for (i = 0; format && format[i]; i++)
 	{
 		if (format[i] == '%' && format[i + 1] != '\0')
 		{
-			j = 0;
-			while (j < 3)
-			{
-				if ((format[i] == '%') && (format[i + 1] == *fm[j].op))
-				{
-					fm[j].f(formato);
-					i++;
-				}
-				else if (format[i + 1] == '%')
-				{
-					_putchar(format[i]);
-					i++;
-					break;
-				}
-				j++;
-			}
+			if (format[i + 1] == ' ')
+				exit(255);
+			else
+				print_func(format, formato);
 		}
 		else if (format[i] == '%' && format[i + 1] == '\0')
 			exit(255);
 		else
-
 			_putchar(format[i]);
-		i++;
 	}
 	va_end(formato);
 	return (0);

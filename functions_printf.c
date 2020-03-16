9f84cbce927d71ca473e199827b4966da9100207
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-16 00:47:31 -0500

    adding conditional

--- functions_printf.c
+++ functions_printf.c
@@ -23,8 +23,6 @@ int print_func(const char *format, argum fm[], va_list formato)
 					break;
 				}
 			}
-			if (format[i] != fm[j].op)
-				exit(255);
 			if (fm[j].op == '\0')
 				cont += _putchar('%');
 		}

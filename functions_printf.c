5378605e00d7b2c40b287fecfb87774e32cb7b5a
Andmontc <oscarandresmontes@gmail.com>
2020-03-16 02:05:19 +0000

    change the amount of functions

--- functions_printf.c
+++ functions_printf.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
-#include "holberton.h"
-#include <stdio.h>
 /**
- * print_function - function print
+ * print_func - function print
  * @format: is a parameter
+ * @fm: struct
  * @formato: is a parameter
  * Return: length of string
  */
@@ -17,7 +16,7 @@ int print_func(const char *format, argum fm[], va_list formato)
 		{
 			for (j = 0; fm[j].op != '\0'; j++)
 			{
-				if ((format[i + 1] == fm[j].op))
+				if (format[i + 1] == fm[j].op)
 				{
 					cont += fm[j].f(formato);
 					i++;
@@ -56,41 +55,4 @@ int _strlen(char *s)
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
-}
-/**
- * print_c - function print caracter
- * @formato: is a parameter to print
- * Return: length of string
- */
-int print_c(va_list formato)
-{
-	int i = 0;
-
-	i = _putchar(va_arg(formato, int));
-	return (i);
-}
-/**
- * print_s - function print string
- * @formato: is a parameter to print
- * Return: length of string
- */
-int print_s(va_list formato)
-{
-	char *ptr = va_arg(formato, char *);
-
-	int len = _strlen(ptr);
-
-	if (ptr == NULL)
-		return (1);
-	write(1, ptr, len);
-
-	return (len);
-}
-/**
- * print_p - function porcent
- */
-int print_p(void)
-{
-	_putchar('%');
-	return(1);
 }

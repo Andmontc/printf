e4a102ff6376e29e3ca6109e9fd7ec5e640f096a
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-18 11:18:27 -0500

    add functions

--- functions_prints0.c
+++ functions_prints0.c
@@ -9,6 +9,7 @@ int print_c(va_list formato)
 	char c;
 
 	c = (char)va_arg(formato, int);
+
 	_putchar(c);
 	return (1);
 }
@@ -27,9 +28,9 @@ int print_s(va_list formato)
 	{
 		ptr = "(null)";
 	}
+	
 	for (j = 0; ptr[j]; j++)
 		_putchar(ptr[j]);
-
 	return (j);
 }
 /**

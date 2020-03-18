d33d15d5d4a59500fc7f3bd615831af7a53c2563
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-03-18 11:40:06 -0500

    add functions

--- functions_prints0.c
+++ functions_prints0.c
@@ -9,7 +9,6 @@ int print_c(va_list formato)
 	char c;
 
 	c = (char)va_arg(formato, int);
-
 	_putchar(c);
 	return (1);
 }
@@ -21,15 +20,15 @@ int print_c(va_list formato)
 int print_s(va_list formato)
 {
 	char *ptr = va_arg(formato, char *);
+	int j;
 
 	if (ptr == NULL)
 		ptr = "(null)";
 
-	int len = _strlen(ptr);
+	for (j = 0; ptr[j]; j++)
+		_putchar(ptr[j]);
 
-	write(1, ptr, len);
-
-	return (len);
+	return (j);
 }
 /**
  * print_p - function porcent
